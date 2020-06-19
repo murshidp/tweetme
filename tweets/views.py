e11beeb7b0ef607e5d4d47c92904789e30e32929
murshidp <murshidparambadan@gmail.com>
2020-06-19 07:04:12 +0530

    view and detail page admin added

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,Http404,JsonResponse 
+from.models import Tweet
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("hello world") 
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404
+    
+    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}")
+
+
+
