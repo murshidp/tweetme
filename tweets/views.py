3c164aa69fc7d969163ffd1732b65172f859457e
murshidp <murshidparambadan@gmail.com>
2020-06-23 01:20:47 +0530

    tweets list view added

--- tweets/views.py
+++ tweets/views.py
@@ -7,6 +7,14 @@ from.models import Tweet
 def home_view(request, *args, **kwargs):
     return render(request,"pages/home.html")
 
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweet_list = [{"id":x.id,"content":x.content} for x in qs]
+    data = {
+        "response":tweet_list
+    }
+    return JsonResponse(data)
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     data = {
         "id":tweet_id,
